9f0984e37f5f24a495dbcf60aacf23f552388c2d
NiharikaNeha <nehaniharikaswain07@gmail.com>
2026-06-29 20:31:33 +0530

    Question-2 completed

--- script.js
+++ script.js
@@ -2,15 +2,16 @@
 // Variables = Username, Email Id, Password, Age, Phone Number
 
 //Question - 2
-let bank_statement = {
-    accountHolderName: "Roshan Sharma",
-    accountNumber: 1234567890,
-    bankName: "Punjab National Bank",
-    statementPeriod: "1st June to 30th June",
-    openingBalance: "1,00,000",
-    description: "UPI, ATM, NEFT, IMPS, cheque",
-    debit: "₹10,000",
-    credit: "₹20,000"
-};
+// let bank_statement = {
+//     accountHolderName: "Roshan Sharma",
+//     accountNumber: 1234567890,
+//     bankName: "Punjab National Bank",
+//     statementPeriod: "1st June to 30th June",
+//     openingBalance: "1,00,000",
+//     description: "UPI, ATM, NEFT, IMPS, cheque",
+//     debit: "₹10,000",
+//     credit: "₹20,000"
+// };
 
-console.log(bank_statement);
+// console.log(bank_statement);
+
